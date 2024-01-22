e7467e0f9d293cfaef0cee710c441ef8f2503306
posh <101807149+ImPosh@users.noreply.github.com>
2024-01-22 11:03:10 -0500

    Update config.js

--- m/config.js
+++ m/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/a/',
-    bare: 'https://refactored-space-broccoli-jpgv9544vxg2rg6-8080.app.github.dev/v/',
+    bare: 'https://savethetrees.sen.org.nz/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: './m/handler.js',
