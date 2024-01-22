941e05f0b75b829017db0ac9d91ce1d742f43947
posh <101807149+ImPosh@users.noreply.github.com>
2024-01-22 11:01:40 -0500

    Update config.js

--- m/config.js
+++ m/config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/uv/service/',
+    prefix: '/a/',
     bare: 'https://refactored-space-broccoli-jpgv9544vxg2rg6-8080.app.github.dev/v/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
