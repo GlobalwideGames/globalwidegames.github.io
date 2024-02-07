de0c1d4a22101e482300ab6c0c9b0fb3386badc9
posh <101807149+ImPosh@users.noreply.github.com>
2024-02-07 13:11:31 -0500

    Update aboutblank.js

--- Assests/JS/aboutblank.js
+++ Assests/JS/aboutblank.js
@@ -19,16 +19,32 @@ function about_blank(){
     window.location.replace("https://google.com");
 }
 
+// Opens page in a new about:blank tab
 function setAboutBlank() {
     const $aboutBlankSelect = document.getElementById('aboutBlankSelect');
     const aboutBlankSelect = $aboutBlankSelect.value;
     if (aboutBlankSelect === 'on') {
         localStorage.setItem('aboutBlank', 'on');
-        about_blank();
+        openAboutBlank();
     } else if (aboutBlankSelect === 'off') {
         localStorage.setItem('aboutBlank', 'off');
     }
 }
+
+// opens page in about:blank
+function openAboutBlank(url) {
+    if (url === undefined) {
+      var encoded_url = window.location.origin;
+    }
+    else {
+      var encoded_url = url;
+    }
+    var w = open('about:blank', '_blank') || alert("It seems like you are blocking pop-ups. Please try again once you have allowed pop-ups.")
+      w.document.write(`<iframe style="height: 100%; width: 100%; border: none;" src="${encoded_url}" allowfullscreen></iframe>`)
+      w.document.body.style.margin = '0'
+    window.location.replace("https://google.com"); 
+}
+// end of about:blank functions
 
 function createCustom() {
   let UrlTingy = document.getElementById("textInputThingy").value;
