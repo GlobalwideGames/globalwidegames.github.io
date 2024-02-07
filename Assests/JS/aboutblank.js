4deb78a1fd6948a2bfe2b50b6205bd53841eda4c
posh <101807149+ImPosh@users.noreply.github.com>
2024-02-07 11:32:57 -0500

    Update aboutblank.js

--- Assests/JS/aboutblank.js
+++ Assests/JS/aboutblank.js
@@ -35,8 +35,16 @@ function WidgetBot() {
 }
 
 function RailinAboutBlank() {
-  window.location.replace("https://google.com");
-  create(protocolThingy);
+   const tab = window.open('about:blank', '_blank');
+    const iframe = tab.document.createElement('iframe');
+    const stl = iframe.style;
+    stl.border = stl.outline = 'none';
+    stl.width = '100vw';
+    stl.height = '100vh';
+    stl.position = 'fixed';
+    stl.left = stl.right = stl.top = stl.bottom = '0';
+    iframe.src = self.location;
+    tab.document.body.appendChild(iframe);
 }
 
 function createCustom() {
