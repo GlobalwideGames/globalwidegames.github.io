6e4df00b712829bca874b2ae62a157a594b533d7
posh <101807149+ImPosh@users.noreply.github.com>
2024-02-07 11:39:09 -0500

    Update aboutblank.js

--- Assests/JS/aboutblank.js
+++ Assests/JS/aboutblank.js
@@ -1,50 +1,21 @@
-const protocolThingy = window.location.protocol + "//" + window.location.host;
-
-function create(urlThingyHere) {
-  var url = urlThingyHere;
-
-  if (url) {
-    var win;
-    if (win) {
-      win.focus();
-    } else {
-      win = window.open();
-      win.document.title = "New Tab";
-      win.document.body.style.margin = "0";
-      win.document.body.style.height = "100vh";
-      var iframe = win.document.createElement("iframe");
-      iframe.style.border = "none";
-      iframe.style.width = "100%";
-      iframe.style.height = "100%";
-      iframe.style.margin = "0";
-      iframe.src = url;
-      win.document.body.appendChild(iframe);
-
-      var interval = setInterval(function () {
-        if (win.closed) {
-          clearInterval(interval);
-          win = undefined;
-        }
-      }, 500);
-    }
-  }
-}
-
 function WidgetBot() {
   create(protocolThingy + "/Assests/Others/WidgetBot.html");
 }
 
-function RailinAboutBlank() {
-   const tab = window.open('about:blank', '_blank');
-    const iframe = tab.document.createElement('iframe');
-    const stl = iframe.style;
-    stl.border = stl.outline = 'none';
-    stl.width = '100vw';
-    stl.height = '100vh';
-    stl.position = 'fixed';
-    stl.left = stl.right = stl.top = stl.bottom = '0';
-    iframe.src = self.location;
-    tab.document.body.appendChild(iframe);
+function about_blank(){
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.body.style.margin = '0';
+    newWindow.document.body.style.height = '100vh';
+    newWindow.document.body.style.minHeight = '100vh';
+    var iframe = newWindow.document.createElement('iframe');
+    iframe.style.border = 'none';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.minWidth = '100%';
+    iframe.style.minHeight = '100%';
+    iframe.style.margin = '0';
+    iframe.src = `${window.location.origin}`;
+    newWindow.document.body.appendChild(iframe);
 }
 
 function createCustom() {
